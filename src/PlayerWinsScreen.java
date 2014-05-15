952fbffedfd125a83d5e98c479317df71309bb45
Katya Babacheva <kbabacheva@gmail.com>
2014-05-16 02:34:30 +0300

    Update PlayerWinsScreen.java

--- src/PlayerWinsScreen.java
+++ src/PlayerWinsScreen.java
@@ -10,7 +10,7 @@ public class PlayerWinsScreen {
 		g.fillRect(0, 0, Pong.WINDOW_WIDTH, Pong.WINDOW_HEIGHT);
 		g.setFont(new Font("TimesRoman", Font.PLAIN, 60)); //Font color
 		g.setColor(Color.WHITE);
-		g.drawString("Game Over",50,80);
+		g.drawString("Game Over!",50,80);
 		g.drawString("You win!",70,180);
 	}
 }
