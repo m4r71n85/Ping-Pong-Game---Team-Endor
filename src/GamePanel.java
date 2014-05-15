b1318c9794dd03699e738a2fc8333cdc364b1ce0
Katya Babacheva <kbabacheva@gmail.com>
2014-05-16 01:46:07 +0300

    Update GamePanel.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.Font; //Font size
 
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener{
